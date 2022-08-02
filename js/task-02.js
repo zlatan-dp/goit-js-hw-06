e12f022659d15bb11c9d19753126c8758c326c11
zlatan-dp <zlatandnepr@gmail.com>
2022-08-02 15:53:35 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,15 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingrList = ingredients.map(ingredient => {
+  const ingrLi = document.createElement('li');
+  ingrLi.textContent = ingredient;
+  ingrLi.classList.add('item');
+
+  return ingrLi;
+});
+
+// console.log(ingrList);
+
+const ingridientsEl = document.querySelector('#ingredients');
+
+ingridientsEl.append(...ingrList);
