476262f8b6e3a5ab9572fc8d230b330095a0080b
zlatan-dp <zlatandnepr@gmail.com>
2022-08-04 12:29:06 +0300

    update task6

---
+++ js/task-06.js
@@ -0,0 +1,12 @@
+const textInput = document.querySelector('#validation-input');
+console.log(textInput.dataset.length);
+
+textInput.addEventListener('blur', evt => {
+  if (evt.currentTarget.value.length >= textInput.dataset.length) {
+    textInput.classList.add('valid');
+    textInput.classList.remove('invalid');
+  } else {
+    textInput.classList.add('invalid');
+    textInput.classList.remove('valid');
+  }
+});
