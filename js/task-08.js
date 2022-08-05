910c2d24adccb2c58848cede5c83c516b358a657
zlatan-dp <zlatandnepr@gmail.com>
2022-08-05 15:07:11 +0300

    update task9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  const formElements = evt.currentTarget.elements;
+  //console.log(formElements);
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const mailName = formElements.email.name;
+  const passwordName = formElements.password.name;
+
+  if (mail === '' || password === '') {
+    return alert('Все поля должны быть заполнены!');
+  }
+
+  //console.log(mail, password);
+  const formValues = {
+    [mailName]: mail,
+    [passwordName]: password,
+  };
+
+  console.log(formValues);
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+colorBtn.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+});
